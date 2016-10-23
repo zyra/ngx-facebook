2faaaec49bd5bafdf41b1d26732ea2cf798569ce
Sebastian Giro <giro.sebastian@gmail.com>
2016-10-23 17:11:10 -0300

    Exports for Ionic2 rc1 support (#20)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export * from './ng2-facebook-sdk';
+export {
+  FacebookService,
+  FacebookInitParams,
+  FacebookApiMethod,
+  FacebookUiParams,
+  FacebookAuthResponse,
+  FacebookLoginStatus,
+  FacebookLoginOptions,
+  FacebookLoginResponse
+} from './ng2-facebook-sdk';
