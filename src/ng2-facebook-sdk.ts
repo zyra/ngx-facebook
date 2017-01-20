c9da0d671d09ed4c3bf262841dbe2d09c3495b71
Ibby <ibby93@gmail.com>
2017-01-20 14:04:59 -0500

    feat(): add grantedScopes property to FacebookAuthResponse
    closes #35

--- src/ng2-facebook-sdk.ts
+++ src/ng2-facebook-sdk.ts
@@ -336,6 +336,10 @@ export interface FacebookAuthResponse {
      * The Facebook user ID
      */
     userID: string;
+    /**
+     * The granted scopes. This field is only available if you set `return_scopes` to true when calling login method.
+     */
+    grantedScopes?: string;
 }
 
 export interface FacebookLoginStatus {
@@ -344,10 +348,25 @@ export interface FacebookLoginStatus {
 }
 
 export interface FacebookLoginOptions {
+    /**
+     * Optional key, only supports one value: rerequest. Use this when re-requesting a declined permission.
+     */
     auth_type?: string;
+    /**
+     * Comma separated list of extended permissions
+     */
     scope?: string;
+    /**
+     * When true, the granted scopes will be returned in a comma-separated list.
+     */
     return_scopes?: boolean;
+    /**
+     * When true, prompt the user to grant permission for one or more Pages.
+     */
     enable_profile_selector?: boolean;
+    /**
+     * Comma separated list of IDs to display in the profile selector
+     */
     profile_selector_ids?: string;
 }
 
