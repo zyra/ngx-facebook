a2fd34c58e09fa92f823cda05451ed4687c8cf16
Ibby <ibby93@gmail.com>
2017-03-20 23:28:11 -0400

    output error if doc generation fails

--- scripts/update_docs.js
+++ scripts/update_docs.js
@@ -9,8 +9,14 @@ const projectPackage = require('../package.json'),
 
 const dgeni = new Dgeni([dgeniConfig]);
 
+try {
   dgeni.generate()
-  .then(docs => {
-    console.log(`${docs.length} docs generated`);
-  })
-  .catch(e => console.log('Error generating docs', e));
+    .then(docs => {
+      console.log(`${docs.length} docs generated`);
+    });
+} catch (e) {
+  console.error('An error occurred while attempting to generate docs');
+  console.error(e.message);
+  console.error(e.stack);
+  process.exit(1);
+}
