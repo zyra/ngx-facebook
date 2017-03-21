8551bab25c22d8fac97e94ffb60e1deeef34f6a4
Ibby <ibby93@gmail.com>
2017-03-20 22:54:36 -0400

    set github config

--- scripts/update_docs.sh
+++ scripts/update_docs.sh
@@ -1,3 +1,5 @@
+git config --global user.email "ibby93+zmbot@gmail.com"
+git config --global user.name "ZM Bot"
 rm -rf ../ng2-facebook-sdk-site
 git clone git@github.com:zyramedia/ng2-facebook-sdk.git ../ng2-facebook-sdk-site
 cd ../ng2-facebook-sdk-site
