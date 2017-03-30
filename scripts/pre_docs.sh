1a4262b0f575598c4527c2cc0a50e0aabf558015
Ibby Hadeed <ibby93@gmail.com>
2017-03-30 19:35:26 -0400

    tests

--- scripts/pre_docs.sh
+++ scripts/pre_docs.sh
@@ -12,14 +12,3 @@ git pull
 git rm -rf *
 
 cd ../ng2-facebook-sdk
-
-npm run docs
-
-cp ../ng2-facebook-sdk/scripts/site_includes/* ../ng2-facebook-sdk-site/
-
-cd ../ng2-facebook-sdk-site
-
-git add .
-git commit -am "Automated docs update"
-
-git push origin gh-pages || true
