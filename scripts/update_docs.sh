9d8ce2a4e36a422a986c8d956a942a0ff246c9e5
Ibby Hadeed <ibby93@gmail.com>
2017-03-30 19:12:00 -0400

    fixes

--- scripts/update_docs.sh
+++ scripts/update_docs.sh
@@ -5,8 +5,10 @@ git clone git@github.com:zyramedia/ng2-facebook-sdk.git ../ng2-facebook-sdk-site
 cd ../ng2-facebook-sdk-site
 git checkout gh-pages
 git pull
-git rm -rf * || true
+git rm -rf *
+cd ../ng2-facebook-sdk
 npm run docs
+cd ../ng2-facebook-sdk-site
 cp ../ng2-facebook-sdk/scripts/site_includes/* .
 git add . || true
 git commit -am "Automated docs update" || true
