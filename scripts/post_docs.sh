897b87672f6124dd6f5ee208486b3a8945294d82
Ibby Hadeed <ibby93@gmail.com>
2017-03-31 04:33:09 -0400

    fix doc gen

--- scripts/post_docs.sh
+++
@@ -1,8 +0,0 @@
-cp ../ng2-facebook-sdk/scripts/site_includes/* ../ng2-facebook-sdk-site/
-
-cd ../ng2-facebook-sdk-site
-
-git add .
-git commit -am "Automated docs update"
-
-git push origin gh-pages || true
