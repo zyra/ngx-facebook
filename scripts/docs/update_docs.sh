0be1c099a6aa298167be3cee03a00950381349f9
Ibby Hadeed <ibby93@gmail.com>
2017-04-19 13:02:23 -0400

    update paths

--- scripts/docs/update_docs.sh
+++ scripts/docs/update_docs.sh
@@ -1,25 +1,25 @@
 git config --global user.email "ibby93+zmbot@gmail.com"
 git config --global user.name "ZM Bot"
 
-rm -rf ../ng2-facebook-sdk-site
+rm -rf ../ngx-facebook-site
 
-git clone git@github.com:zyramedia/ng2-facebook-sdk.git ../ng2-facebook-sdk-site
-cd ../ng2-facebook-sdk-site
+git clone git@github.com:zyra/ngx-facebook.git ../ngx-facebook-site
+cd ../ngx-facebook-site
 
 git checkout gh-pages
 git pull
 
 git rm -rf *
 
-cd ../ng2-facebook-sdk
+cd ../ngx-facebook
 
 npm run docs
 
-cp scripts/docs/site_includes/* circle.yml ../ng2-facebook-sdk-site/
+cp scripts/docs/site_includes/* circle.yml ../ngx-facebook-site/
 
-cd ../ng2-facebook-sdk-site
+cd ../ngx-facebook-site
 
 git add .
-git commit -am "Automated docs update zyramedia/ng2-facebook-sdk@$CIRCLE_SHA1" -m "[ci skip]"
+git commit -am "Automated docs update zyra/ngx-facebook@$CIRCLE_SHA1" -m "[ci skip]"
 
 git push origin gh-pages || true
