3b920520d87d134b62ac579ee8ab69ef2babe166
Daniel <mytechde@outlook.com>
2018-09-24 12:03:53 +0200

    WIP

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -29,10 +29,7 @@ module.exports = function (config) {
     customLaunchers: {
       Headless_Chrome: {
         base: 'Chrome',
-        flags: [
-          '--no-sandbox',
-          '--disable-gpu'
-        ]
+        flags: ['--no-sandbox', '--disable-gpu']
       }
     },
     singleRun: false
